4c80ba419431e93f2da011f6765a786dd218a0fa
alex5758 <142025614+alex5758@users.noreply.github.com>
2026-03-14 23:23:57 +0500

    Refactor bot commands and message handling

--- telegram_bot_izzat.py
+++ telegram_bot_izzat.py
@@ -1,48 +1,56 @@
 import os
 import telebot
 from telebot import types
+
+# Берем токен из переменной окружения
 bot = telebot.TeleBot(os.getenv("BOT_TOKEN"))
-@bot.message_handler(commands = ['start'])
-def url(message):
-	markup = types.InlineKeyboardMarkup()
-	btn1 = types.InlineKeyboardButton(text = 'Наш сайт', url = 'https://habr.com/ru/all/')
-	markup.add(btn1)
-	bot.send_message(message.from_user.id, "По кнопке ниже можно перейти на сайт хабра", reply_markup = markup)
 
-
-@bot.message_handler(commands = ['start'])
+# Обработчик команды /start
+@bot.message_handler(commands=['start'])
 def start(message):
-	markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
-	btn1 = types.KeyboardButton("ru Русский")
-	btn2 = types.KeyboardButton("gb English")
-	markup.add(btn1, btn2)
-	bot.send_message(message.from_user.id, "ru Выберете язык / gb Choose your language", reply_markup = markup)
-@bot.message_handler(commands = ['start'])
-def start(message):
-	markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
-	btn1 = types.KeyboardButton("Поздороваться")
-	markup.add(btn1)
-	bot.send_message(message.from_user.id, "Привет! Я твой бот-помощник!", reply_markup = markup)
+    chat_id = message.chat.id
 
-@bot.message_handler(content_types = ['text'])
-def get_text_messages(message):
-	if message.text == 'Поздороваться':
-		markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
-		btn1 = types.KeyboardButton('Как стать автором на Хабре?')
-		btn2 = types.KeyboardButton('Правила сайта')
-		btn3 = types.KeyboardButton('Советы по оформлению публикации')
-		markup.add(btn1, btn2, btn3)
-		bot.send_message(message.from_user.id, 'Задайте интересующий вопрос', reply_markup = markup)
-	elif message.text == 'Как стать автором на Хабре?':
-		bot.send_message(message.from_user.id, 'Вы пишите пост, его ' + '[ссылке](https://habr.com/ru/sandbox/start/)', parse_mode = 'Markdown')
-	elif message.text == 'Правила сайта':
-		bot.send_message(message.from_user.id, 'Прочитать правила сайта ' + '[ссылке](https://habr.com/ru/docs/help/rules/)', parse_mode = 'Markdown')
-	elif message.text == 'Советы по оформлению публикации':
-		bot.send_message(message.from_user.id, 'Подробно' + '[ссылке](https://habr.com/ru/docs/companies/design/)', parse_mode = 'Markdown')
-bot.polling(none_stop = True, interval = 0)
-		
+    # 1️⃣ Inline-кнопка на сайт
+    inline = types.InlineKeyboardMarkup()
+    btn_site = types.InlineKeyboardButton(text='Наш сайт', url='https://habr.com/ru/all/')
+    inline.add(btn_site)
+    bot.send_message(chat_id, "По кнопке ниже можно перейти на сайт Хабра:", reply_markup=inline)
 
+    # 2️⃣ Выбор языка через ReplyKeyboard
+    markup_lang = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn_ru = types.KeyboardButton("ru Русский")
+    btn_gb = types.KeyboardButton("gb English")
+    markup_lang.add(btn_ru, btn_gb)
+    bot.send_message(chat_id, "Выберите язык / Choose your language:", reply_markup=markup_lang)
 
+# Обработка текстовых сообщений
+@bot.message_handler(content_types=['text'])
+def handle_text(message):
+    chat_id = message.chat.id
+    text = message.text
 
+    # Выбор языка
+    if text == "ru Русский":
+        bot.send_message(chat_id, "Вы выбрали русский язык 🇷🇺")
+    elif text == "gb English":
+        bot.send_message(chat_id, "You chose English 🇬🇧")
 
+    # Поздороваться
+    elif text == "Поздороваться":
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton("Как стать автором на Хабре?")
+        btn2 = types.KeyboardButton("Правила сайта")
+        btn3 = types.KeyboardButton("Советы по оформлению публикации")
+        markup.add(btn1, btn2, btn3)
+        bot.send_message(chat_id, "Задайте интересующий вопрос:", reply_markup=markup)
 
+    # Ответы на вопросы
+    elif text == "Как стать автором на Хабре?":
+        bot.send_message(chat_id, "Вы можете начать здесь: [ссылка](https://habr.com/ru/sandbox/start/)", parse_mode="MarkdownV2")
+    elif text == "Правила сайта":
+        bot.send_message(chat_id, "Прочитать правила сайта: [ссылка](https://habr.com/ru/docs/help/rules/)", parse_mode="MarkdownV2")
+    elif text == "Советы по оформлению публикации":
+        bot.send_message(chat_id, "Полезная информация: [ссылка](https://habr.com/ru/docs/companies/design/)", parse_mode="MarkdownV2")
+
+# Запуск бота
+bot.polling(none_stop=True, interval=0)
